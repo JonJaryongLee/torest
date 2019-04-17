ace7cb5bcdb977e204afd98179d41b15ad34e035
Jon Jaryong Lee <director9303@gmail.com>
2019-04-17 10:43:42 +0900

    0417

--- src/components/MyForest/MyForest.vue
+++ src/components/MyForest/MyForest.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
-        <p>MyForest</p>
-        <p>MyForest</p>
+        <div class="mapContainer">
+            <div class="mapParentItem" v-for="n in 3" :key="n">
+                <div class="mapChildItem" v-for="m in 3" :key="m"></div>
+            </div>
+        </div>
     </div>
 </template>
-
 <script>
-	export default{
+export default {
 
-	}
+}
 </script>
+<style scoped>
+.mapContainer {
+    position: relative;
+    top: 100px;
+    left: 400px;
+    display: flex;
+    border: 1px solid gray;
+    height: 400px;
+    width: 400px;
+    transform: rotateX(50deg) rotate(45deg) ;
+}
+
+.mapParentItem {
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+}
+
+.mapChildItem {
+    border: 1px solid blue;
+    flex-grow: 1;
+}
+</style>
