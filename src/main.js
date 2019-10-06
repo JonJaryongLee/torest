c08ac5b7f25863249cfa6865285486de7deccf31
Jon Jaryong Lee <director9303@gmail.com>
2019-10-06 15:22:21 +0900

    뷰라우터 설치

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import './plugins/vuetify'
 import App from './App.vue'
 import VueChartkick from 'vue-chartkick'
 import Chart from 'chart.js'
 Vue.use(VueChartkick, {adapter: Chart})
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
